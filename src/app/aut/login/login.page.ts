3b5bfab52bd3a5b66866b8c77200b237ddafc84f
Moehid23 <muthadoabdul23@gmail.com>
2024-06-04 22:24:24 +0700

    footer

--- src/app/aut/login/login.page.ts
+++ src/app/aut/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -13,20 +14,35 @@ export class LoginPage {
     password: ''
   };
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private toastController: ToastController
+  ) { }
 
   login() {
     this.http.post<any>('http://127.0.0.1:8000/api/login', this.loginData)
       .subscribe(
-        (response) => {
+        async (response) => {
           console.log('Login successful', response);
           localStorage.setItem('access_token', response.access_token);
+          await this.showToast('Login berhasil!', 'success');
           this.router.navigateByUrl('/homepage');
         },
-        (error) => {
+        async (error) => {
           console.error('Login failed', error);
-          // Tampilkan pesan kesalahan kepada pengguna
+          await this.showToast('Login gagal. Periksa kembali email dan password Anda.', 'danger');
         }
       );
+  }
+
+  async showToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: color,
+      position: 'top' // Menentukan posisi toast di tengah layar
+    });
+    await toast.present();
   }
 }
